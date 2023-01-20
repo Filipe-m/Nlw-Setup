52c17486f62455ae1d7c216a87269124fbeda792
Filipe Moreira Coelho <60034638+Filipe-m@users.noreply.github.com>
2023-01-20 10:03:09 -0300

    summary days UI

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,15 @@
 /* import Habit from './components/Habit' */
-import { Header } from './components/header';
-import './styles/global.css';
+import { Header } from './components/header'
+import { SummaryTable } from './components/summaryTable'
+import './styles/global.css'
 
 export function App() {
-
   return (
     <div className=" w-screen h-screen flex justify-center items-center  ">
-      <div className=' w-full max-w-5xl px-6 flex flex-col gap-16 ' >
+      <div className=" w-full max-w-5xl px-6 flex flex-col gap-16 ">
         <Header />
+        <SummaryTable />
       </div>
     </div>
   )
 }
-
